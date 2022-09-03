3e67e7e18b5ce47dcb47ab05ccba4e0b0a0475dd
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-09-03 19:50:45 +0200

    Connect to MySQL database at startup

--- app.js
+++ app.js
@@ -2,19 +2,33 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const mysql2 = require('mysql2');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+connection = mysql2.createConnection({
+  host: process.env.mysql_host || 'localhost',
+  user: process.env.mysql_user,
+  password: process.env.mysql_password,
+  database: process.env.mysql_database
+});
+
+connection.connect(function (e) {
+  if (e) {
+    return console.error('error: ' + e.message);
+  }
+
+  console.log('\nConnected to the MySQL server\n');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,7 +41,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
