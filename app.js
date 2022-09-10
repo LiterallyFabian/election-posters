8237824e7e2ba5240b6c9d0716478f32fe83f9ef
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-09-10 14:17:39 +0200

    Remove unneeded modules

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var sassMiddleware = require('node-sass-middleware');
 const mysql2 = require('mysql2');
 require('dotenv').config();
 
@@ -30,13 +28,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
